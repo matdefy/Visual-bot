521d705786cfda6277e18f16232de0c64efb6d8c
matdefy <49235849+matdefy@users.noreply.github.com>
2020-08-31 15:18:37 +0200

    verif serveur channel

--- commands/creation.js
+++ commands/creation.js
@@ -3,27 +3,33 @@ const config = require('C:/Users/matte/montage_video/bot/Graph-bot/config.json')
 
 module.exports = {
     run: (db, message, args) => {
-        var creationId = 1
-        if (db.has(message.author.id)) {
-            creationId = db.get(message.author.id).length + 1
-        }
-        if (message.attachments.size === 1) {
-            db.push(message.author.id, {
-                id: creationId,
-                url: message.attachments.first().url,
-                verif: '❌'
-            })
+        if (message.guild.id === '747834737527226542' && message.channel.name.startsWith('ticket')) {
+            var creationId = 1
+            if (db.has(message.author.id)) {
+                creationId = db.get(message.author.id).length + 1
+            }
+            if (message.attachments.size === 1) {
+                db.push(message.author.id, {
+                    id: creationId,
+                    url: message.attachments.first().url,
+                    verif: '❌'
+                })
 
-            message.channel.send(new Discord.MessageEmbed()
-                .setDescription('✅ Création enregistrée ✅')
-                .setColor('#00FF00')
-                .setFooter(config.version, 'https://cdn.discordapp.com/attachments/577866353030201355/749204835982770206/logo2_8.png'))
+                message.channel.send(new Discord.MessageEmbed()
+                    .setDescription('✅ Création enregistrée ✅')
+                    .setColor('#00FF00')
+                    .setFooter(config.version, 'https://cdn.discordapp.com/attachments/577866353030201355/749204835982770206/logo2_8.png'))
+            } else {
+                message.channel.send(new Discord.MessageEmbed()
+                    .setDescription('❌ Veuillez entrer 1 création ❌')
+                    .setColor('#FF0000')
+                    .setFooter(config.version, 'https://cdn.discordapp.com/attachments/577866353030201355/749204835982770206/logo2_8.png'))
+            }
         } else {
             message.channel.send(new Discord.MessageEmbed()
-                .setDescription('❌ Veuillez entrer 1 création ❌')
+                .setDescription('❌ Veuillez entrer cette commande dans les channels de tickets sur ce [serveur](https://discord.gg/Xs4kThY) ❌')
                 .setColor('#FF0000')
                 .setFooter(config.version, 'https://cdn.discordapp.com/attachments/577866353030201355/749204835982770206/logo2_8.png'))
         }
     }
-
 }
--- commands/preuve.js
+++ commands/preuve.js
@@ -3,33 +3,40 @@ const config = require('C:/Users/matte/montage_video/bot/Graph-bot/config.json')
 
 module.exports = {
     run: (db, message, args, client) => {
-        var creationIdPr = args[0]
-        if (db.has(message.member.id)) {
-            const creationIdverif = db.get(message.author.id).some((creation) => creation.id === parseInt(args[0]))
-            if (message.attachments.size === 1) {
-                if (creationIdverif) {
-                    db.push('pr_' + message.author.id, {
-                        id: creationIdPr,
-                        url: message.attachments.first().url
-                    })
-                    message.channel.send(new Discord.MessageEmbed()
-                        .setDescription('✅ Preuve enregistrée ✅')
-                        .setColor('#00FF00')
-                        .setFooter(config.version, 'https://cdn.discordapp.com/attachments/577866353030201355/749204835982770206/logo2_8.png'))
+        if (message.guild.id === '747834737527226542' && message.channel.name.startsWith('ticket')) {
+            var creationIdPr = args[0]
+            if (db.has(message.member.id)) {
+                const creationIdverif = db.get(message.author.id).some((creation) => creation.id === parseInt(args[0]))
+                if (message.attachments.size === 1) {
+                    if (creationIdverif) {
+                        db.push('pr_' + message.author.id, {
+                            id: creationIdPr,
+                            url: message.attachments.first().url
+                        })
+                        message.channel.send(new Discord.MessageEmbed()
+                            .setDescription('✅ Preuve enregistrée ✅')
+                            .setColor('#00FF00')
+                            .setFooter(config.version, 'https://cdn.discordapp.com/attachments/577866353030201355/749204835982770206/logo2_8.png'))
+                    } else {
+                        message.channel.send(new Discord.MessageEmbed()
+                            .setDescription('❌ Création introuvable ❌')
+                            .setColor('#00FF00')
+                            .setFooter(config.version, 'https://cdn.discordapp.com/attachments/577866353030201355/749204835982770206/logo2_8.png'))
+                    }
                 } else {
                     message.channel.send(new Discord.MessageEmbed()
-                        .setDescription('❌ Création introuvable ❌')
-                        .setColor('#00FF00')
+                        .setDescription('❌ Veuillez entrer 1 preuve ❌')
+                        .setColor('#FF0000')
                         .setFooter(config.version, 'https://cdn.discordapp.com/attachments/577866353030201355/749204835982770206/logo2_8.png'))
                 }
             } else {
-                message.channel.send(new Discord.MessageEmbed()
-                    .setDescription('❌ Veuillez entrer 1 preuve ❌')
-                    .setColor('#FF0000')
-                    .setFooter(config.version, 'https://cdn.discordapp.com/attachments/577866353030201355/749204835982770206/logo2_8.png'))
+
             }
         } else {
-
+            message.channel.send(new Discord.MessageEmbed()
+                .setDescription('❌ Veuillez entrer cette commande dans les channels de tickets sur ce [serveur](https://discord.gg/Xs4kThY) ❌')
+                .setColor('#FF0000')
+                .setFooter(config.version, 'https://cdn.discordapp.com/attachments/577866353030201355/749204835982770206/logo2_8.png'))
         }
     }
 }
