8274a6e069cc8d99c44afd063d5942e5f0dc09a7
matdefy <49235849+matdefy@users.noreply.github.com>
2020-10-31 18:11:48 +0100

    ✨ : ajout de 3 commandes

---
+++ commands/eval.js
@@ -0,0 +1,29 @@
+module.exports = {
+    run: (db, message, args, client, dbLogs) => {
+        if (message.author.id === '364481003479105537' || message.author.id === '422820341791064085') {
+            const content = message.content
+                .split(' ')
+                .slice(1)
+                .join(' ')
+            // eslint-disable-next-line no-eval
+            const result = new Promise(resolve => resolve(eval(content)))
+            return result
+                .then(output => {
+                    if (typeof output !== 'string') {
+                        output = require('util').inspect(output, { depth: 0 })
+                    }
+                    if (output.includes(message.client.token)) {
+                        output = output.replace(message.client.token, 'T0K3N')
+                    }
+                    return message.channel.send(output, { code: 'js' })
+                })
+                .catch(err => {
+                    err = err.toString()
+                    if (err.includes(message.client.token)) {
+                        err = err.replace(message.client.token, 'T0K3N')
+                    }
+                    return message.channel.send(err, { code: 'js' })
+                })
+        }
+    }
+}
