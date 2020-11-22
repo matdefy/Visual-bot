a434a81e7edd3c0ce9e7750de08228d9b87c6c0a
matdefy <49235849+matdefy@users.noreply.github.com>
2020-11-22 16:55:04 +0100

    ✨ : le bot permet maintenant d'envoyer des messages MP choisi (pour les recrutements du serveur principal) (ne fonctionne que avec 1 personne) !

--- commands/say.js
+++ commands/say.js
@@ -4,9 +4,14 @@ const config = require('../config.json')
 module.exports = {
     run: (db, message, args, client) => {
         if (message.author.id === '364481003479105537') {
+            const userID = args[0]
+            const descriptCMD = message.content.trim().slice(`${config.prefix}cmd ${userID} `.length)
             if (args[0]) {
                 message.delete()
-                message.channel.send(message.content.trim().slice(`${config.prefix}say`.length))
+                client.users.cache.get(userID).send(new Discord.MessageEmbed()
+                    .setDescription(descriptCMD)
+                    .setColor('#00FF00')
+                    .setFooter(config.version, client.user.avatarURL()))
             } else {
                 message.channel.send(new Discord.MessageEmbed()
                     .setDescription('⚠️ Veuiller entrer du texte ⚠️')
