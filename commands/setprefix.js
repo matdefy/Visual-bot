92e68b2e78726ec048c1ad07ba1acfc3a5d502e1
matdefy <49235849+matdefy@users.noreply.github.com>
2020-11-29 11:50:44 +0100

    💬 : ajout d'une alerte quand aucun préfix n'est rentré !

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -5,12 +5,19 @@ module.exports = {
     run: (db, message, args, client) => {
         if (message.member.hasPermission('KICK_MEMBERS')) {
             const prefix = args[0]
-            if (prefix.length < 6) {
-                db.set('prefix_' + message.guild.id, prefix)
-                message.channel.send(new Discord.MessageEmbed()
-                    .setDescription('✅ Le préfix pour le serveur : ' + message.guild.name + ' et maintenant : `' + prefix + '` ! ✅\n\n**[Documentation](https://graphbot.gitbook.io/graph-bot/)**')
-                    .setColor('#00FF00')
-                    .setFooter(config.version, message.client.user.avatarURL()))
+            if (args[0]) {
+                if (prefix.length < 6) {
+                    db.set('prefix_' + message.guild.id, prefix)
+                    message.channel.send(new Discord.MessageEmbed()
+                        .setDescription('✅ Le préfix pour le serveur : ' + message.guild.name + ' et maintenant : `' + prefix + '` ! ✅\n\n**[Documentation](https://graphbot.gitbook.io/graph-bot/)**')
+                        .setColor('#00FF00')
+                        .setFooter(config.version, message.client.user.avatarURL()))
+                } else {
+                    message.channel.send(new Discord.MessageEmbed()
+                        .setDescription('⚠️ Le préfix doit faire au maximum 5 caractères ! ⚠️\n\n**[Documentation](https://graphbot.gitbook.io/graph-bot/)**')
+                        .setColor('#e55f2a')
+                        .setFooter(config.version, message.client.user.avatarURL()))
+                }
             } else {
                 message.channel.send(new Discord.MessageEmbed()
                     .setDescription('⚠️ Le préfix doit faire au maximum 5 caractères ! ⚠️\n\n**[Documentation](https://graphbot.gitbook.io/graph-bot/)**')
