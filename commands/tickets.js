0e67d604fdca7a3f7861f79fc33f72881afc0368
matdefy <49235849+matdefy@users.noreply.github.com>
2021-01-16 18:42:46 +0100

    ✨ : système de tickets manuel ajouté

---
+++ commands/tickets.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js')
+const config = require('../config.json')
+
+module.exports = {
+    run: (db, message, args, client) => {
+        const descriptcmd = message.content.trim().slice(`${config.prefix}tickets `.length)
+        // création de la catégorie
+        message.guild.channels.create('tickets ' + descriptcmd, {
+            type: 'category'
+        }).then((categorie) => {
+            const idparent = categorie.id
+            message.channel.send({
+                embed: new Discord.MessageEmbed()
+                    .setDescription('📮 **Création ticket ' + descriptcmd + '**\n\n**Pour créer un ticket, cliquer sur la réaction ☑️**\n**Pour fermer le système de ticket, cliquer sur la réaction 🔒**')
+                    .setColor('#00FF00')
+                    .setFooter(config.version, message.client.user.avatarURL())
+            }).then(msg => {
+                msg.react('☑️')
+                msg.react('🔒')
+                db.push('parentticket_' + message.guild.id, {
+                    idparent: idparent,
+                    urlmessage: msg.url
+                })
+            })
+            message.delete()
+        })
+        // création de la catégorie
+    }
+}
