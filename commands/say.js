62cf24e29957e3b1262a7e6fe8b56815601277cc
matdefy <49235849+matdefy@users.noreply.github.com>
2021-02-09 15:21:04 +0100

    ✨ update say

--- commands/say.js
+++ commands/say.js
@@ -4,19 +4,23 @@ const config = require('../config.json')
 module.exports = {
     run: (db, message, args, client) => {
         if (message.author.id === '364481003479105537') {
-            const descriptCMD = message.content.trim().slice(`${config.prefix}say `.length)
+            const userID = args[0]
+            const descriptCMD = message.content.trim().slice(`${config.prefix}cmd ${userID} `.length)
             if (args[0]) {
                 message.delete()
-                message.channel.send(descriptCMD)
+                client.users.cache.get(userID).send(new Discord.MessageEmbed()
+                    .setDescription(descriptCMD)
+                    .setColor('#FF0000')
+                    .setFooter(config.version, client.user.avatarURL()))
             } else {
                 message.channel.send(new Discord.MessageEmbed()
-                    .setDescription('⚠️ **Veuiller entrer du texte**')
+                    .setDescription('⚠️ Veuiller entrer du texte ⚠️')
                     .setColor('#00FF00')
                     .setFooter(config.version, message.client.user.avatarURL()))
             }
         } else {
             message.channel.send(new Discord.MessageEmbed()
-                .setDescription('🛑 **Vous n\'avez pas les permissions suffisantes**\n\n**(Pour obtenir de l\'aide, une **[documentation](https://graphbot.gitbook.io/graph-bot/)** est disponible !)**')
+                .setDescription('🛑 Vous n\'avez pas les permissions suffisantes ! 🛑')
                 .setColor('#00FF00')
                 .setFooter(config.version, message.client.user.avatarURL()))
         }
